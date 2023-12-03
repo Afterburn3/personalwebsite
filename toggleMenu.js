e312c380d0e9cfdaee343332ff5e31b78380bf0a
KimThai <148199726+Afterburn3@users.noreply.github.com>
2023-12-03 23:40:24 +1100

    Website Demo
    Add JS and Demo 1.
    
    Need to clean JS script, layout for contact and experience, clean up for mobile user,

---
+++ toggleMenu.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".screeen-list ul");
+    const icon = document.querySelector(".button-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
