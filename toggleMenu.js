1d14b5f50cea5370494686ad9f88e6f13c37b7df
KimThai <148199726+Afterburn3@users.noreply.github.com>
2023-12-15 19:25:29 +1100

    Delete toggleMenu.js

--- toggleMenu.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".screeen-list ul");
-    const icon = document.querySelector(".button-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
